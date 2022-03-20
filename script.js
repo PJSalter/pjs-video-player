70eb30aa8b67f0683429930b8972ffe5b05bb4a0
PJSalter <psalter88@googlemail.com>
2022-03-20 02:44:04 +0000

    DOM to pick up video element, function to play and pause video and event listener to handle the click of the video which will cause the function to start

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
 //***************** JavaScript Here ********************//
+
+// picking up the video element with DOM Manipulation.
+const graphicalTranformClip = document.querySelector('video');
+
+// creating function to play and pause video.
+const vidControls = () => {
+    graphicalTranformClip.paused ? graphicalTranformClip.play() : graphicalTranformClip.pause();
+}
+
+//event listener for user to click the video to play or pause
+graphicalTranformClip.addEventListener('click', vidControls);

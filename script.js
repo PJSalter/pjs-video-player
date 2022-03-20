11e48d8a7905681d57a7c7085e6fa39b88041b45
PJSalter <psalter88@googlemail.com>
2022-03-20 22:32:42 +0000

    function added to stop icon and event listener for click

--- script.js
+++ script.js
@@ -11,6 +11,12 @@ const vidControls = () => {
     graphicalTranformClip.paused ? graphicalTranformClip.play() : graphicalTranformClip.pause();
 }
 
+// function for allow video to stop once user clicks on stop icon.
+const stopMyVideo = () => graphicalTranformClip.pause();
+  
 
 //event listener for user to click the video to play or pause
 graphicalTranformClip.addEventListener('click', vidControls);
+
+//event listener to stop video once clicked
+stopMe.addEventListener('click', stopMyVideo);

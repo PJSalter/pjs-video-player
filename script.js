c7dbfb924289da6ddc068454d64cba3c3c6600d4
PJSalter <psalter88@googlemail.com>
2022-03-20 22:25:56 +0000

    DOM to pick up id attribute of stop icon

--- script.js
+++ script.js
@@ -3,10 +3,14 @@
 // picking up the video element with DOM Manipulation.
 const graphicalTranformClip = document.querySelector('video');
 
+// picking up the stop button icon from the controls
+const stopMe = document.getElementById('stop-me');
+
 // creating function to play and pause video.
 const vidControls = () => {
     graphicalTranformClip.paused ? graphicalTranformClip.play() : graphicalTranformClip.pause();
 }
+
 
 //event listener for user to click the video to play or pause
 graphicalTranformClip.addEventListener('click', vidControls);
